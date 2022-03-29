c65b6722b08a2ebe26da2bcfb4042cba230d7754
mnikolov6 <mn04905228@edu1.pgzaimov.com>
2022-03-29 09:30:39 +0300

    Test commit (But added 1 VM)

---
+++ Sladko_Izkushenie/Models/CategoryVM.cs
@@ -0,0 +1,15 @@
+﻿using Sladko_Izkushenie.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sladko_Izkushenie.Models
+{
+    public class CategoryVM
+    {
+        public int Id { get; set; }
+        public string Category_Type { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
