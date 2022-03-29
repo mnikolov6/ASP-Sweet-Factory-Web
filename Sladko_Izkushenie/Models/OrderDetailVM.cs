9220edf4c9ae0fdca9f4d551a1953ccfcb3ee446
mnikolov6 <mn04905228@edu1.pgzaimov.com>
2022-03-29 09:39:48 +0300

    Adding VM's

---
+++ Sladko_Izkushenie/Models/OrderDetailVM.cs
@@ -0,0 +1,18 @@
+﻿using Sladko_Izkushenie.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sladko_Izkushenie.Models
+{
+    public class OrderDetailVM
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public float Quantity { get; set; }
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+    }
+}
---
+++ Sladko_Izkushenie/Models/OrderVM.cs
@@ -0,0 +1,17 @@
+﻿using Sladko_Izkushenie.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sladko_Izkushenie.Models
+{
+    public class OrderVM
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public User User { get; set; }
+        public DateTime Order_date { get; set; }
+        public ICollection<OrderDetail> OrderDetails { get; set; }
+    }
+}
